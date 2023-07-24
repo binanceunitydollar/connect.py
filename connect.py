32bcb321230bfb2a8425b4db9aba5cb912d32489
binanceunitydollar <127662801+binanceunitydollar@users.noreply.github.com>
2023-07-24 19:24:49 +0500

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,21 +1,36 @@
 import sys
 import socket
 
-def connect_to_ip(192.168.0.111, 5555):
+def connect_to_server(ip, port):
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((ip, port))
-        print("Connected to {}:{}".format(ip, port))
-        # Perform further actions as needed
-        sock.close()
+        # Create a TCP socket
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect to the server
+        client_socket.connect((ip, port))
+
+        print(f"Connected to {ip}:{port}")
+
+        # Send data to the server (optional)
+        # client_socket.sendall(b"Hello, server!")
+
+        # Receive data from the server (optional)
+        # data = client_socket.recv(1024)
+        # print("Received:", data.decode())
+
+        # Close the socket
+        client_socket.close()
+
     except ConnectionRefusedError:
-        print("Connection refused to {}:{}".format(ip, port))
+        print("Connection refused. Check if the server is running and the IP and port are correct.")
+    except Exception as e:
+        print("Error:", e)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: python connect.py <ip> <port>")
+    if len(sys.argv) != 3:
+        print("Usage: python connect.py <server_ip> <server_port>")
     else:
-        ip = sys.argv[1]
-        port = int(sys.argv[2])
-        connect_to_ip(ip, port)
-
+        server_ip = sys.argv[1]
+        server_port = int(sys.argv[2])
+        connect_to_server(server_ip, server_port)
+        
